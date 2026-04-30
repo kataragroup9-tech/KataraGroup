1e7609aae50311edbe76544c5f0a4d61eba7f7d5
Sharad Dubey <ersharad.dubey@gmail.com>
2026-04-30 13:30:34 +0530

    Configure MongoDB connection using env variables

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Ye line sabse upar honi chahiye
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
+    process.exit(1);
+  }
+};
